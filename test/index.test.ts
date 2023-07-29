79439a2b064b9b2a1bb4f1378a55d7a86adf5687
jazielloureiro <jazielloureiro17@gmail.com>
2023-07-29 10:56:12 -0300

    Add users endpoint tests

---
+++ test/index.test.ts
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import express from 'express';
+import dataSource from '../src/dataSource';
+import router from '../src/router';
+import userRepository from '../src/repositories/userRepository';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+const userData = { username: 'john', password: '123' };
+
+beforeAll(async () => {
+    await dataSource.initialize();
+});
+
+beforeEach(async () => {
+    await userRepository.createQueryBuilder().delete().from('User').execute();
+});
+
+afterAll(async () => {
+    await dataSource.destroy();
+});
+
+describe('E2E tests', () => {
+    it('POST /users', async () => {
+        const response = await request(app).post('/users').send(userData);
+        const users = await userRepository.find();
+
+        expect(response.statusCode).toBe(201);
+        expect(users.length).toBe(1);
+        expect(users[0].username).toBe(userData.username);
+        expect(users[0].password).not.toBe(userData.password);
+    });
+
+    it('POST /users/login', async () => {
+        await request(app).post('/users').send(userData);
+
+        const response = await request(app).post('/users/login').send(userData);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('token');
+    });
+});
