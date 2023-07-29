e5cce1c770a6dd8a0e225fc4f8a83f06c23e10d5
jazielloureiro <jazielloureiro17@gmail.com>
2023-07-29 16:06:56 -0300

    Add POST /posts endpoint test

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import express from 'express';
 import dataSource from '../src/dataSource';
 import router from '../src/router';
+import postRepository from '../src/repositories/postRepository';
 import userRepository from '../src/repositories/userRepository';
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
+const postData = { content: 'Awesome text' };
 const userData = { username: 'john', password: '123' };
 
 beforeAll(async () => {
@@ -16,6 +18,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+    await postRepository.createQueryBuilder().delete().from('Post').execute();
     await userRepository.createQueryBuilder().delete().from('User').execute();
 });
 
@@ -24,6 +27,25 @@ afterAll(async () => {
 });
 
 describe('E2E tests', () => {
+    it('POST /posts', async () => {
+        await request(app).post('/users').send(userData);
+
+        const loginResponse = await request(app)
+            .post('/users/login')
+            .send(userData);
+
+        const response = await request(app)
+            .post('/posts')
+            .auth(loginResponse.body.token, { type: 'bearer' })
+            .send(postData);
+
+        const posts = await postRepository.find();
+
+        expect(response.statusCode).toBe(201);
+        expect(posts.length).toBe(1);
+        expect(posts[0].content).toBe(postData.content);
+    });
+
     it('POST /users', async () => {
         const response = await request(app).post('/users').send(userData);
         const users = await userRepository.find();
